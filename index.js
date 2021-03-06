d94e6d0ea7f62d9f73ce3ddf46f25171febddd73
EerieAlchemist <66958528+EerieAlchemist@users.noreply.github.com>
2021-03-06 02:38:42 -0500

    added configurable buttons to config

--- index.js
+++ index.js
@@ -15,11 +15,16 @@ rpc.on("ready", () => {
         largeImageKey: config.presence_settings.largeImageKey,
         largeImageText: config.presence_settings.largeImageText,
         smallImageKey: config.presence_settings.smallImageKey,
-        smallImageText: config.presence_settings.smallImageText
-    });
-    console.log("Active.");
+        smallImageText: config.presence_settings.smallImageText,
+        //joinSecret: config.presence_settings.joinSecret,
+        //spectateSecret: config.presence_settings.spectateSecret,
+        buttons: [{
+            label: config.presence_settings.button1_label,
+            url: config.presence_settings.button1_url
+        }, {label: config.presence_settings.button2_label, url: config.presence_settings.button2_url}]
+    }).then(r => console.log("Active."));
 });
 
 rpc.login({
     clientId: config.rpc_login.clientId // your client ID, NOT your Discord user ID!
-});
+}).then(r => "Client logged in.");
